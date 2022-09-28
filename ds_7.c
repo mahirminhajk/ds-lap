b668032ea28d9795fa32d16b0c68291a859f2c57
mahirminhajk <kmahirminhaj@gmail.com>
2022-09-28 22:13:52 +0530

    ds_7

---
+++ ds_7.c
@@ -0,0 +1,46 @@
+/* append two array*/
+
+#include <stdio.h>
+#include <string.h>
+
+void display(int a[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        printf("%d\t", a[i]);
+    }
+    printf("\n");
+}
+
+void main()
+{
+    int a[10], b[10], c[20], a_s, b_s, i, c_s;
+
+    printf("Enter size of the array: \n");
+    scanf("%d", &a_s);
+    printf("Enter the first array %d value\n", a_s);
+    for (i = 0; i < a_s; i++)
+    {
+        scanf("%d", &a[i]);
+        c[i] = a[i];
+    }
+    c_s = a_s;
+
+    printf("Enter size of the array to append: \n");
+    scanf("%d", &b_s);
+
+    printf("Enter the second array %d value\n", a_s);
+    for (i = 0; i < b_s; i++)
+    {
+        scanf("%d", &b[i]);
+        c[c_s] = b[i];
+        c_s++;
+    }
+
+    printf("array before append\n");
+    display(a, a_s);
+
+    printf("array after append: \n");
+    display(c, c_s);
+}
