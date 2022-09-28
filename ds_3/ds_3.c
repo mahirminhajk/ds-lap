49593383a0c92d0f8435f4709835b7ab129278d2
mahirminhajk <kmahirminhaj@gmail.com>
2022-09-28 16:39:09 +0530

    updated ds_3

--- ds_3/ds_3.c
+++ ds_3/ds_3.c
@@ -1,60 +1,28 @@
-//*implement sparse matrix
-// 0   1  2
-// 2   0  2
-// 9   8  2
+/*implement sparse matrix*/
 
-// 0   0  9
-// 8   0  0
-// 0   0  2
 #include <stdio.h>
-
-void display(int a[10][10], int r, int c);
 
 void main()
 {
-    int a[10][10], b[10][10], r = 3, c, x = 0, y = 0, i, j, lr = 1, lc = 1;
+    int a[10][10], r, c, i, j, count = 0;
 
-    printf("Enter the total non-zero elements: \n");
-    scanf("%d", &c);
+    printf("Enter the row and column: \n");
+    scanf("%d %d", &r, &c);
 
-    printf("Enter the matrix:\n");
+    printf("Enter element of the matrix: \n");
     for (i = 0; i < r; i++)
     {
         for (j = 0; j < c; j++)
         {
             scanf("%d", &a[i][j]);
-            b[i][j] = 0;
-        }
-    }
-    printf("matrix entered: \n");
-    display(a, r, c);
-
-    // c = total values
-    for (i = 0; i < 1; i++)
-    {
-        for (j = 0; j < c; j++)
-        {
-            x = a[i][j];
-            y = a[i + 1][j];
-            b[x][y] = a[i + 2][j];
-            if (x > lr)
+            if (a[i][j] == 0)
             {
-                lr = x;
-            }
-            if (y > lc)
-            {
-                lc = y;
+                count++;
             }
         }
     }
 
-    printf("Sparse matrix: \n");
-    display(b, lr + 1, lc + 1);
-}
-
-void display(int a[10][10], int r, int c)
-{
-    int i, j;
+    printf("Elements are:\n");
     for (i = 0; i < r; i++)
     {
         for (j = 0; j < c; j++)
@@ -62,5 +30,14 @@ void display(int a[10][10], int r, int c)
             printf("%d\t", a[i][j]);
         }
         printf("\n");
+    }
+
+    if (count > ((r * c) / 2))
+    {
+        printf("matrix is sparse matrix\n");
+    }
+    else
+    {
+        printf("matirx is not sparse matrix\n");
     }
 }
